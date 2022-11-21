2a27e16d101c2c5e905da6748b59927895d7fc66
Martin Zibricky <mzibrick@redhat.com>
2022-11-21 10:56:42 +0100

    [CCXDEV-5899] Enable loading of exporter in ephemeral in pr_check

--- pr_check.sh
+++ pr_check.sh
@@ -6,7 +6,7 @@
 APP_NAME="ccx-data-pipeline"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="insights-results-aggregator-exporter"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/insights-results-aggregator-exporter"
-COMPONENTS="insights-results-aggregator-exporter ccx-notification-service ccx-notification-db-cleaner"  # space-separated list of components to laod
+COMPONENTS="ccx-insights-results insights-results-aggregator-exporter ccx-notification-service ccx-notification-db-cleaner"  # space-separated list of components to laod
 COMPONENTS_W_RESOURCES="insights-results-aggregator-exporter"  # component to keep
 CACHE_FROM_LATEST_IMAGE="true"
 
@@ -18,33 +18,29 @@ export IQE_REQUIREMENTS_PRIORITY=""
 export IQE_TEST_IMPORTANCE=""
 export IQE_CJI_TIMEOUT="30m"
 
-# Temporary stub
-mkdir artifacts
-echo '<?xml version="1.0" encoding="utf-8"?><testsuites><testsuite name="pytest" errors="0" failures="0" skipped="0" tests="1" time="0.014" timestamp="2021-05-13T07:54:11.934144" hostname="thinkpad-t480s"><testcase classname="test" name="test_stub" time="0.000" /></testsuite></testsuites>' > artifacts/junit-stub.xml
 
-# TODO: Uncomment when there is any test
+function build_image() {
+    source $CICD_ROOT/build.sh
+}
 
-# function build_image() {
-#     source $CICD_ROOT/build.sh
-# }
+function deploy_ephemeral() {
+    source $CICD_ROOT/deploy_ephemeral_env.sh
+}
 
-# function deploy_ephemeral() {
-#     source $CICD_ROOT/deploy_ephemeral_env.sh
-# }
-
-# function run_smoke_tests() {
-#     source $CICD_ROOT/cji_smoke_test.sh
-# }
+function run_smoke_tests() {
+    source $CICD_ROOT/cji_smoke_test.sh
+    source $CICD_ROOT/post_test_results.sh  # publish results in Ibutsu
+}
 
 
-# # Install bonfire repo/initialize
-# CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-# curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
-# echo "creating PR image"
-# build_image
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+echo "creating PR image"
+build_image
 
-# echo "deploying to ephemeral"
-# deploy_ephemeral
+echo "deploying to ephemeral"
+deploy_ephemeral
 
-# echo "running PR smoke tests"
-# run_smoke_tests
+echo "running PR smoke tests"
+run_smoke_tests
